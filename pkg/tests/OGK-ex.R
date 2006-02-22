fb9b01d9e0484a60bf72b9c6d26677c61833db35
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2006-02-22 15:55:31 +0000

    misc (MM)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@30 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/tests/OGK-ex.R
+++ pkg/tests/OGK-ex.R
@@ -20,5 +20,30 @@ stopifnot(AE(c(rub1$wcov)[c(1,3:4)],
              c(927.2465953711782, 91.8009184487779, 2346.5790105548940))
           )
 
-## More tests:
-## set.seed(101)
+q()
+
+## More tests: ------------ not yet / too slow --> do other place ------------
+
+set.seed(101)
+library(MASS)# mvrnorm()
+N <- 1000
+p <- 50
+
+mu <- rep(0, p)
+## p x p  cov.matrix with const. sigma=10
+S0 <- toeplitz(10* ARMAacf(0.8, lag.max = p - 1))
+
+
+nSim <- 20
+r <- vector("list", nSim)
+cpu <- numeric(nSim)
+for(i in 1:nSim) {
+    X <- mvrnorm(N, mu = mu, Sigma = S0)
+    cpu[i] <-
+        system.time(cc <- covOGK(X, n.it = 2,
+                                 sigmamu = scaleTau2, weight.fn = hard.rejection))
+    r[[i]] <- cc
+}
+
+
+cat('Time elapsed: ', proc.time(),'\n') # for ``statistical reasons''
