76283fd0a1c49893a3c04bf966924f260986d901
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2007-08-02 21:34:26 +0000

    fixing adjOutlyingness()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@127 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/R/adjoutlyingness.R
+++ pkg/R/adjoutlyingness.R
@@ -1,3 +1,5 @@
+#### -*- mode: R; kept-new-versions: 30; kept-old-versions: 20 -*-
+
 #### MC-adjusted Outlyingness
 #### ------------------------
 ###
@@ -10,7 +12,7 @@
 ## MM [ FIXME ]:
 ## -----------
 
-## 1)   Use  *transposed*  B[] and A[] matrices
+## 1)   Use  *transposed*  B[] and A[] matrices   -- done
 
 ## 2)   use  IQR() instead of   quantile(., .75) - quantile(., .25)
 
@@ -18,36 +20,38 @@
 ##     ^^^^^^^^^^^^^^^^^^^^^^^^
 
 adjOutlyingness <- function(x, ndir=250, clower=3, cupper=4,
-                            alpha.cutoff = 0.75, coef = 1.5)
+                            alpha.cutoff = 0.75, coef = 1.5, qr.tol = 1e-12)
 ## Skewness-Adjusted Outlyingness
 {
     x <- data.matrix(x)
     n <- nrow(x)
     p <- ncol(x)
-    stopifnot(n > 0, p > 0, is.numeric(x))
+    stopifnot(n >= 1, p >= 1, is.numeric(x))
     if (p < n) {
         i <- 1
         it <- 0
-        B <- matrix(0,nrow = ndir,ncol = p)
-        A <- matrix(0,nrow = ndir,ncol = p)
-        while (i <= ndir  &&  (it <- it+1) < 1000) {
-            xx <- sample(n)
-            P <- x[xx[1:p],]
-            if (qr(P, 1e-12)$rank == p) {
-                B[i,] <- solve(P,matrix(1,nrow = p,ncol = 1))
+        B <- matrix(0, p, ndir)
+        E <- matrix(1, p, 1)
+        x. <- unname(x) # for speed in subsequent subsetting and solve
+        maxit <- 100 * ndir
+        ## ^^ original code had 'Inf', i.e. no iter.count check;
+        ## often, maxit == ndir would suffice
+        while (i <= ndir  &&  (it <- it+1) < maxit) {
+            P <- x.[sample(n,p), , drop=FALSE]
+            if ((qrP <- qr(P, tol = qr.tol))$rank == p) {
+                B[,i] <- solve(qrP, E, tol =  qr.tol)
                 i <- i+1
             }
         }
-        Bnorm <- matrix(0,nrow = ndir,ncol = 1)
-        for (i in 1:ndir) {
-            Bnorm[i] <- sum(abs(B[i,])^2)^(1/2)
-        }
-        Bnormr <- Bnorm[Bnorm > 1e-12]
-        B      <-     B[Bnorm > 1e-12,]
- ## FIXME: no loop :
-        for (i in 1:ndir) {
-            A[i,] <- Bnormr[i]^(-1)*B[i,]
-        }
+        if(it == maxit)
+            stop("**** sampling iterations were not sufficient. Please report")
+
+        Bnorm <- sqrt(colSums(B^2))
+        ## MM: FIXME '1e-12' is not ok scale-equivariant!
+        Bnormr <- Bnorm[  Bnorm > 1e-12]
+        B      <-     B[, Bnorm > 1e-12 , drop=FALSE]
+
+        A <- B / rep(Bnormr, each = nrow(B))
     }
     else {
         stop('More dimensions than observations: not yet implemented')
@@ -67,37 +71,53 @@ adjOutlyingness <- function(x, ndir=250, clower=3, cupper=4,
         ##    A=diag(1./Bnormr)*B;         %ndirect*n
 
     }
-    Y <- x %*% t(A)
-    YZ <- matrix(0, nrow = n, ncol = ndir)
+    Y <- x %*% A # (n x p) %*% (p, ndir) == (n x ndir)
+
+    ## Compute and sweep out the median
+    med <- apply(Y, MARGIN = 2, median)
+    Y <- Y - rep(med, each=n)
+    ## MM: mc() could be made faster if we could tell it that med(..) = 0
     tmc <- apply(Y, MARGIN = 2, mc)
     ##                          ==
-    ## MM: Quart <- apply(Y, quantile, c(1:3)/4, names = FALSE)
-    tme <- apply(Y, MARGIN = 2, median)
-    tp3 <- apply(Y, MARGIN = 2, quantile,0.75)
-    tp1 <- apply(Y, MARGIN = 2, quantile,0.25)
-    tiq <- tp3-tp1
-    tup <- -tme +(tp3 + coef*tiq*exp( cupper*tmc*(tmc >= 0)-clower*tmc*(tmc < 0)))
-    tlo <- tme - (tp1 - coef*tiq*exp(-clower*tmc*(tmc >= 0)+cupper*tmc*(tmc < 0)))
-    for (i in 1:ndir) {
-        tup[i] <- -min(-Y[Y[,i] < (tup[i]+tme[i]),i])-tme[i]
-        tlo[i] <- tme[i]-min(Y[Y[,i] > (tme[i]-tlo[i])])
-    }
-## FIXME (without loop!):
-    for (j in 1:n) {
-        YZ[j,] <- abs(Y[j,]-tme)/((Y[j,] >= tme)*tup+(Y[j,] < tme)*tlo)
-    }
-    adjout <- apply(YZ, MARGIN = 1, max)
-    Qadj <- quantile(adjout, probs = c(1 - alpha.cutoff, alpha.cutoff))
+    Q3 <-  apply(Y, MARGIN = 2, quantile, 0.75)
+    Q1 <-  apply(Y, MARGIN = 2, quantile, 0.25)
+    IQR <- Q3-Q1
+    ## NOTA BENE(MM): simplified definition of tup/tlo here and below
+    tup <- Q3 + coef*IQR*exp( cupper*tmc*(tmc >= 0) - clower*tmc*(tmc < 0))
+    tlo <- Q1 - coef*IQR*exp(-clower*tmc*(tmc >= 0) + cupper*tmc*(tmc < 0))
+    ## Note: all(tlo < med & med < tup)
 
+    ## Instead of the loop:
+    ##  for (i in 1:ndir) {
+    ##      tup[i] <-  max(Y[Y[,i] < tup[i], i])
+    ##      tlo[i] <- -min(Y[Y[,i] > tlo[i], i])
+    ##      ## MM:  FIXED typo-bug :       ^^^ this was missing!
+    ##      ## But after the fix, the function stops "working" for longley..
+    ##      ## because tlo[] becomes 0 too often, YZ[.,.] = c / 0 = Inf !
+    ##  }
+    Yup <- Ylo <- Y
+    Yup[!(Y < rep(tup, each=n))] <- -Inf
+    Ylo[!(Y > rep(tlo, each=n))] <-  Inf
+    tup <-  apply(Yup, 2, max) # =  max{ Y[i,] ; Y[i,] < tup[i] }
+    tlo <- -apply(Ylo, 2, min) # = -min{ Y[i,] ; Y[i,] > tlo[i] }
+
+    tY <- t(Y)
+    Ypos <- (tY >= 0) ## note that all column-wise medians are 0
+    ## Note: this loop is pretty fast
+    for (j in 1:n)
+        tY[, j] <- abs(tY[,j]) / (Ypos[,j]*tup + (1 - Ypos[,j])*tlo)
+    ## FIXME -- what if denominator is 0 ? happens often in small samples
+    ##	 e.g in  set.seed(3); adjOutlyingness(longley)
+    ## even  have  0/0 -> NaN there  --> is.finite(.) below.. hmm, FIXME!
+
+    adjout <- apply(tY, 2, function(x) max(x[is.finite(x)]))
+    Qadj <- quantile(adjout, probs = c(1 - alpha.cutoff, alpha.cutoff))
     mcadjout <- mc(adjout)
     ##          ===
-    if (mcadjout > 0) {
-        cutoff <- Qadj[2]+ coef*exp(cupper*mcadjout)*(Qadj[2]-Qadj[1])
-    }
-    else {
-        cutoff <- Qadj[2]+ coef*(Qadj[2]-Qadj[1])
-    }
+    cutoff <- Qadj[2] + coef* (Qadj[2] - Qadj[1])*
+        (if(mcadjout > 0) exp(cupper*mcadjout) else 1)
 
-    list(adjout = adjout, cutoff = cutoff,
-         flag = (adjout <= cutoff))
+    list(adjout = adjout,
+	 MCadjout = mcadjout, Qalph.adjout = Qadj, cutoff = cutoff,
+	 nonOut = (adjout <= cutoff))
 }
