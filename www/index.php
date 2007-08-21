57bf2a7c76456bb098e7e7a144f8af73bed059de
mmaechler <mmaechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2007-08-21 21:27:23 +0000

    manually adding www
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@131 694ef91d-65df-0310-b7bb-92e67a308ead

---
+++ www/index.php
@@ -0,0 +1,58 @@
+
+<!-- This is the project specific website template -->
+<!-- It can be changed as liked or replaced by other content -->
+
+<?php
+
+$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
+$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
+$themeroot='http://r-forge.r-project.org/themes/rforge/';
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
+
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title><?php echo $group_name; ?></title>
+	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+  </head>
+
+<body>
+
+<! --- R-Forge Logo --- >
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tr><td>
+<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+</table>
+
+
+<!-- get project title  -->
+<!-- own website starts here, the following may be changed as you like -->
+
+<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
+$contents = '';
+while (!feof($handle)) {
+	$contents .= fread($handle, 8192);
+}
+fclose($handle);
+echo $contents; } ?>
+
+<!-- end of project description -->
+
+<p> One important aspect of this package:
+ Though it is maintained by Martin Maechler,
+the idea is that the whole "robustnik" community contributes to it
+<b>and</b> builds its own R packages with a <tt>'Depends: robustbase'</tt>
+entry, i.e., reusing all the example datasets and basic robustness functions
+procedures from robustbase.
+</p>
+
+<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+
+</body>
+</html>
