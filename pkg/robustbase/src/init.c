80cce83867e3d0a7d9f793c294b472ac176880e2
valentint <valentint@694ef91d-65df-0310-b7bb-92e67a308ead>
2010-12-02 23:02:38 +0000

    Changes in ltsReg (.R, .f, .Rd) - raw weights added, trace parameter added, fixed the options nsamp="exact" and nsamp="best"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@226 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/robustbase/src/init.c
+++ pkg/robustbase/src/init.c
@@ -91,7 +91,7 @@ static const R_CMethodDef CEntries[]  = {
 
 static R_FortranMethodDef FortEntries[] = {
     {"rffastmcd", (DL_FUNC) &F77_SUB(rffastmcd), 48},/* ./rffastmcd.f */
-    {"rfltsreg",  (DL_FUNC) &F77_SUB(rfltsreg), 41}, /* ./rfltsreg.f */
+    {"rfltsreg",  (DL_FUNC) &F77_SUB(rfltsreg), 42}, /* ./rfltsreg.f */
     {NULL, NULL, 0}
 };
 
