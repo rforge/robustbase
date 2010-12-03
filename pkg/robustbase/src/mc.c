555b1e1bf58eceecdd5a3401b56abab065de4181
kollerma <kollerma@694ef91d-65df-0310-b7bb-92e67a308ead>
2010-12-03 07:58:18 +0000

    Reversing change from REprintf to Rprintf
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@228 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/robustbase/src/mc.c
+++ pkg/robustbase/src/mc.c
@@ -251,7 +251,7 @@ double mc_C_d(double *z, int n, double *eps, int *iter)
 
     converged = IsFound || (nr-nl <= n);
     if(!converged) {
-	Rprintf("maximal number of iterations (%d =? %d) reached prematurely\n",
+	REprintf("maximal number of iterations (%d =? %d) reached prematurely\n",
 		 iter[0], it);
 	/* still: */
 	medc = trial;
