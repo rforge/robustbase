0aa552f50b2c9672f0f39b05ee1db09db4443500
mmaechler <mmaechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2010-12-04 21:56:52 +0000

    another Qn (finite sample correction) round
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@230 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/robustbase/R/qnsn.R
+++ pkg/robustbase/R/qnsn.R
@@ -3,7 +3,12 @@
 ### correct constant == 1 / (sqrt(2) * qnorm(5/8)) == 2.219144
 ### -- but wrong constant, 2.2219, is already in the the original Fortran qn.f
 Qn.corr <- 2.2219 / 2.21914
-##
+##' Qn finite sample correction factor  (not exported, but "available")
+Qn.finite.c <- function(n)
+    (if (n %% 2) 1.6069 +(-2.333 - 3.1/n)/n # n  odd
+     else        3.6667 +( 2.591 - 5.5/n)/n # n  even
+    )/n + 1
+
 Qn <- function(x, constant = 2.21914, finite.corr = missing(constant))
 {
     ## Purpose: Rousseeuw and Croux's  Q_n() robust scale estimator
@@ -14,16 +19,31 @@ Qn <- function(x, constant = 2.21914, finite.corr = missing(constant))
     r <- constant *
         .C(Qn0, as.double(x), n, res = double(1))$res
 
+    if (finite.corr) {
+	if (n <= 10) ## n in 2:10 --> n-1 in 1:9
+	    ## n=2: E[Q_2] = E|X - Y| = sqrt(pi)/2, fc = sqrt(pi)/2/2.21914
+	    r* c(.399356,.9936,.5132, .8442,
+		 .6123, .8590, .6700, .8734, .7201)[n - 1L]
+	else
+	    r / Qn.finite.c(n)
+    }
+    else r
+}
+
+## This is the old version -- available for back "compatibility":
+Qn.old <- function(x, constant = 2.2219, finite.corr = missing(constant))
+{
+    ## Purpose: Rousseeuw and Croux's  Q_n() robust scale estimator
+    ## Author: Martin Maechler, Date: 14 Mar 2002, 10:43
+    n <- length(x)
+    if(n == 0) return(NA) else if(n == 1) return(0.)
+
+    r <- constant *
+        .C(Qn0, as.double(x), n, res = double(1))$res
+
     if (finite.corr)
-	(if (n <= 9) {
-	    if      (n == 2)  .399
-	    else if (n == 3)  .995
-	    else if (n == 4)  .513
-	    else if (n == 5)  .845
-	    else if (n == 6)  .612
-	    else if (n == 7)  .858
-	    else if (n == 8)  .670
-	    else if (n == 9)  .873
+	(if (n <= 9) { # n in 2:9 --> n-1 in 1:8
+            c(.399,.994, .512,.844, .611,.857, .669,.872)[n - 1]
 	} else {
 	    if (n %% 2) ## n odd
 		n / (n + 1.4)
@@ -32,17 +52,6 @@ Qn <- function(x, constant = 2.21914, finite.corr = missing(constant))
         }
          ) * r
     else r
-}
-
-if(FALSE) {
-## How to correct the 9 finite sample correction factors which were from simulations
-## computed with the original wrong constant?
-## We need  [const * fc]_{new} == [const * fc]_{old}.
-## ==> fc_{new} = fc_old *  (const_{old} / const_{new}) = fc_old * Qn.corr
-Qn.fin.9.old <- ## original finite sample corrections for n = 2:9:
-    c(.399, .994, .512, .844, .611, .857, .669, .872)
-round(Qn.fin.9.old * Qn.corr, 3)
-##-> 0.399 0.995 0.513 0.845 0.612 0.858 0.670 0.873
 }
 
 
