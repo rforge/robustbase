89b9ed27905144e39259b40fa6aafc94b0ca17e6
kollerma <kollerma@694ef91d-65df-0310-b7bb-92e67a308ead>
2011-01-18 17:00:28 +0000

    psiFunc testcase for default argument problem of E... slots
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@239 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/robustbase/tests/psi-rho-etc.R
+++ pkg/robustbase/tests/psi-rho-etc.R
@@ -13,3 +13,16 @@ stopifnot(EQ(tukeyChi(x, c.),
              6/c.^2* tukeyPsi1(x, c., deriv= 0)),
           EQ(tukeyChi(x, c., deriv= 2),
              6/c.^2* tukeyPsi1(x, c., deriv= 1)))
+
+## Test if default arguments are used
+tPsi <- chgDefaults(huberPsi, k = 2)
+
+stopifnot(tPsi@rho(1:10, k=2) == tPsi@rho(1:10),
+          tPsi@psi(1:10, k=2) == tPsi@psi(1:10),
+          tPsi@Dpsi(1:10, k=2) == tPsi@Dpsi(1:10),
+          tPsi@wgt(1:10, k=2) == tPsi@wgt(1:10))
+
+## FIXME: Default arguments are not used for E... slots
+try(tPsi@Erho())
+try(tPsi@Epsi2())
+try(tPsi@EDpsi())
