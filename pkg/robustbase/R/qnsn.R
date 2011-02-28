b9be271abdc2f9c490602d7a0512b6ff58769b84
mmaechler <mmaechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2011-02-28 10:05:34 +0000

    Qn() finite-sample correction from (2 weeks ago); some JSS URLs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@249 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/robustbase/R/qnsn.R
+++ pkg/robustbase/R/qnsn.R
@@ -4,9 +4,15 @@
 ### -- but wrong constant, 2.2219, is already in the the original Fortran qn.f
 Qn.corr <- 2.2219 / 2.21914
 ##' Qn finite sample correction factor  (not exported, but "available")
+##' Version 1
 Qn.finite.c <- function(n)
     (if (n %% 2) 1.6069 +(-2.333 - 3.1/n)/n # n  odd
      else        3.6667 +( 2.591 - 5.5/n)/n # n  even
+    )/n + 1
+## Version built on res <- cbind(Res.sml, Res.mid) ## and the models there
+Qn.finite.c <- function(n)
+    (if (n %% 2) 1.60188 +(-2.1284 - 5.172/n)/n # n  odd
+     else        3.67561 +( 1.9654 +(6.987 - 77/n)/n)/n # n  even
     )/n + 1
 
 Qn <- function(x, constant = 2.21914, finite.corr = missing(constant))
@@ -20,10 +26,13 @@ Qn <- function(x, constant = 2.21914, finite.corr = missing(constant))
         .C(Qn0, as.double(x), n, res = double(1))$res
 
     if (finite.corr) {
-	if (n <= 10) ## n in 2:10 --> n-1 in 1:9
+	if (n <= 12) ## n in 2:12 --> n-1 in 1:11
 	    ## n=2: E[Q_2] = E|X - Y| = sqrt(pi)/2, fc = sqrt(pi)/2/2.21914
-	    r* c(.399356,.9936,.5132, .8442,
-		 .6123, .8590, .6700, .8734, .7201)[n - 1L]
+	    r* c(.399356, # ~= fc = 0.3993560233
+                 ## These are from MM's simulation("Res3"), Nsim = 2^27 ~= 134 mio:
+                 .99365, .51321, .84401, .61220,
+                 .85877, .66993, .87344, .72014,
+                 .88906, .75743)[n - 1L]
 	else
 	    r / Qn.finite.c(n)
     }
