9692ea08c83d50851d8ff91b113175f996bf66a8
mmaechler <mmaechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2014-12-03 19:57:12 +0000

    add covComed() [after testing / compatibility / minimal testing]
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@663 694ef91d-65df-0310-b7bb-92e67a308ead

---
+++ pkg/robustbase/R/comedian.R
@@ -0,0 +1,135 @@
+### -*- mode: R ; delete-old-versions: never -*-
+
+### This program is free software; you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation; either version 2 of the License, or
+### (at your option) any later version.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with this program; if not, write to the Free Software
+### Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+### Maria Anna di Palma, without consistency factor 15.11.2014
+### Fixes by Valentin Todorov
+### Martin Maechler: added mad() consistency factor, 27.11.2014
+###     new name, class; more compatible to 'covMcd'
+
+covComed <- function (X, n.iter = 2, reweight = FALSE,
+                      tolSolve = control$ tolSolve,# had 1e-10 hardwired {now 1e-14 default}
+                      trace = control$ trace,
+                      wgtFUN = control$ wgtFUN,
+                      control = rrcov.control())
+{
+    ## ATTENTION ##
+    ## Med(abs(X))^2=Med(X*X) only if the number of rows is odd
+    d <- dim(X <- as.matrix(X))
+    n <- d[1]
+    p <- d[2]
+
+    if(is.character(wgtFUN))
+	wgtFUN <- .wgtFUN.covMcd[[wgtFUN]](p)
+    if(!is.function(wgtFUN))
+	stop("'wgtFUN' must be a function or a string specifying such a function")
+
+    madX <- apply(X, 2, mad)
+    I.mad <- 1/madX
+    rho <- I.mad * COM(X) * rep(I.mad, each = p)
+    ## better than
+    ## D <- diag(1/madX)
+    ## rho <- D %*% COM(X) %*% t(D)
+
+    U <- svd(rho, p, nv = 0L)$u
+    ## DD <- diag(madX)
+    ## Q <- DD %*% U
+    ## invQ <- solve(Q) ##  == t(U) %*% D -- since  U is orthogonal!
+    t.inv.Q <- I.mad * U # = t(solve(Q)) = t(t(U) * D) == t(D) U = D U
+    Z <- X %*% t.inv.Q ## much faster than for (i in 1:n) Z[i,] <- invQ %*% X[i,]
+    out <- comedian(rho, Z, X)
+
+    ## Mahalanobis distance
+    for(it in seq_len(n.iter))# allow n.iter = 0
+        out <- comedian(out$S., out$Z, X)
+
+    mm <- apply(out$Z, 2, median)
+    mx <- t(out$Q %*% mm)
+    ## MM: These are "raw" distances compared to covMcd()
+    mah <- mahalanobis(X, mx, out$S., tol = tolSolve)
+
+    ## compute weights
+    weights <- wgtFUN(mah)
+    covW <- cov.wt(X, wt=weights)[c("cov", "center", "n.obs")]
+    covW$weights <-
+	if(reweight) { ## above 'mah' = 'raw.mah' .. ==> allow another reweighting as in covMcd()
+	    covW$raw.weights <- weights
+	    covW$mah <- mahalanobis(X, covW$center, covW$cov, tol = tolSolve)
+	    wgtFUN(mah)
+	} else # no re-weighting
+	    weights
+    structure(class = "comed",
+	      c(list(Z = out$Z, raw.cov = out$S., raw.center = mx, raw.mah = mah,
+		     wgtFUN=wgtFUN),
+		covW))
+}
+
+
+## Default wgtFUN() constructors for covComed():
+.wgtFUN.covComed <-
+    list("01.original" = function(p) {
+             cMah <- 1.4826 * qchisq(0.95,p) / qchisq(0.5,p)
+             function(d) as.numeric(d < median(d)*cMah) }
+        ,
+         "01.flex" = function(p, beta) {
+             cMah <- 1.4826 * qchisq(beta, p) / qchisq(0.5, p)
+             function(d) as.numeric(d < median(d)*cMah) },
+        ,
+         "M1.flex" = function(p, beta) {
+             cMah <- 1.4826 * qchisq(beta, p) / qchisq(0.5, p)
+             function(d) as.numeric(d < median(d)*cMah) },
+         NULL)
+
+
+comedian <- function (rho, Z, X)
+{
+    p <- ncol(X)
+    U <- svd(rho, nv = 0L)$u
+    madX <- apply(X, 2, mad)
+    I.mad <- 1/madX
+    ## D <- diag(madX)
+    ## Q <- D %*% U
+    Q <- madX * U
+    ## invQ <- solve(Q)
+    t.inv.Q <- I.mad * U # = t(solve(Q)) = t(t(U) * D) == t(D) U = D U
+    Z <- X %*% t.inv.Q ## for (i in 1:n)  Z[i,] <- invQ %*% X[i,]
+    madZ <- apply(Z, 2, mad)
+    list(Q=Q, Z=Z, S. = tcrossprod(Q * rep(madZ, each=p))) ## better than
+    ##             S. = Q %*% diag(madZ)^2 %*% t(Q)
+}
+
+COM <- function(X)
+{
+    ## Comedian *with* consistency factor.  Falk(1997) was without it.
+
+    stopifnot(length(p <- ncol(X)) == 1, p >= 1)
+    med <- apply(X, 2L, median)
+    Y <- sweep(X, 2L, med, `-`)
+    COM <- matrix(0., p,p)
+    madY <- numeric(p)
+    for(i in 1:p) {
+        madY[i] <- madYi <- mad(Yi <- Y[,i])
+        for(j in seq_len(i-1)) { # j <= i ==> madY[j] "exists"
+            COM[j,i] <- COM[i,j] <- median(Yi * Y[,j]) / (madYi * madY[j])
+            ## COM[i,j] <- median((Y[,i])*(Y[,j]))
+            ## COM[i,j] <- (1.4826^2)*median((Y[,i])*(Y[,j]))
+        }
+        ## j == i :
+        COM[i,i] <- median(Yi^2) / (madYi^2)
+    }
+    ## return [ 1.4826 = formals(mad)$constant = consistency factor of mad()]
+    1.4826^2 * COM
+}
