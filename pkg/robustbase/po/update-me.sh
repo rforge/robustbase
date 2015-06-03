a91524abc2a1e62d26cb2eb451f0727e57a64db2
mmaechler <mmaechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2015-06-03 09:21:46 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@696 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/robustbase/po/update-me.sh
+++ pkg/robustbase/po/update-me.sh
@@ -12,15 +12,12 @@ echo "R = '$R' (`$R --version | head -1`)
 pkgDIR=`dirname $thisdir`
 pkg=`basename $pkgDIR`
 echo '  -->        pkgDIR='$pkgDIR' ; pkg='$pkg
-# echo ''; echo '## FIXME ## use new Scheme from R 3.0.x on'
-# cd `$R RHOME`/po
-# make pkg-update PKG=$pkg PKGDIR=$pkgDIR
 echo "require('tools'); update_pkg_po('$pkgDIR')" | $R --slave 2>&1 | tee update.log
 ##    --------------------------------  as of R 3.0.0
 echo 'end{make pkg-update}' ; echo ''
 echo 'Test with (e.g.)'
 echo '       LANGUAGE=de R --no-environ --no-save' ; echo ''
 echo 'and then something like'
-echo '       ellipsoidPoints(diag(3)) ; ellipsoidPoints(cbind(1,1:3))'; echo ''
+echo '       ... [TO BE ADDED] ...'; echo ''
 echo 'Commit with something like'
 echo "       svn ci -m'translation updates'  po  inst/po"; echo ''
