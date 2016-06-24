56751101201e5f09fddb2a961ede1e7f06172e7f
mmaechler <mmaechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2016-06-24 05:12:10 +0000

    complete r719 [print( summary ( nlrob(*, "CM") ) )]
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@720 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/robustbase/R/nlrob.R
+++ pkg/robustbase/R/nlrob.R
@@ -365,7 +365,8 @@ summary.nlrob <- function (object, correlation = FALSE, symbolic.cor = FALSE, ..
                      "call", "status", "counts", "iter", "control", "ctrl"), no, 0L)]
     ans <- object[no]
     conv <- ans$status == "converged"
-    sc   <- sigma(ans)# = ans$Scale but not always
+    if(is.null(sc <- ans$Scale))
+	ans$Scale <- sc <- sigma(object)
     if(conv && !is.matrix(ans$cov))
 	ans$cov <- .vcov.m(object, Scale = sc,
 			   resid.sc = as.vector(object$residuals) / sc)
@@ -454,7 +455,8 @@ print.summary.nlrob <-
 	    else ## length(it) >= 2 :
 		cat("Convergence\n\n")
 	}
-	summarizeRobWeights(x$rweights, digits = digits, ...)
+	if(!is.null(x$rweights))
+	    summarizeRobWeights(x$rweights, digits = digits, ...)
     }
     else if(meth == "M")
 	cat("** IRWLS iterations did *not* converge!\n\n")
