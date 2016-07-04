5c390b84561ec3b00af7df1c5c7efd8247614107
mmaechler <mmaechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2016-07-04 22:16:31 +0000

    show the "KS20[14]" problem for starsCYG data
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@725 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/robustbase/tests/lmrob-data.R
+++ pkg/robustbase/tests/lmrob-data.R
@@ -27,10 +27,47 @@ data(coleman)
 summary( m1 <- lmrob(Y ~ ., data=coleman) )
 stopifnot(c(3,18) == which(m1$w < 0.2))
 
+if(FALSE) # to find out *why setting = "KS201x" fails
+trace(lmrob.S, exit = quote({cat("coef:\n"); print(b$coefficients)}))
+if(FALSE) # to find out via  setting = "KS201x" fails here in the *initial* estimate
+debug(lmrob.S)
+
 data(starsCYG)
-(RlmST <- lmrob(log.light ~ log.Te, data = starsCYG))
+  lmST <-    lm(log.light ~ log.Te, data = starsCYG)
+(RlmST <- lmrob(log.light ~ log.Te, data = starsCYG, control=lmrob.control(trace = 1)))
 summary(RlmST)
-stopifnot(c(11,20,30,34) == which(RlmST$w < 0.01))
+## Least Sq. w/ negative slope, where robust has slope ~= 2.2 :
+stopifnot(coef(lmST)[["log.Te"]] < 0,
+          all.equal(coef(RlmST), c("(Intercept)" = -4.969, log.Te=2.253), tol = 1e-3),
+          c(11,20,30,34) == which(RlmST$w < 0.01))
+## ==> Now see that  "KS2011" and "KS2014" both break down -- and it is the fault of "lqq" *only* :
+(RlmST.11 <- update(RlmST, control = lmrob.control("KS2011",                             trace= 1)))
+(RlmST.14 <- update(RlmST, control = lmrob.control("KS2014",                             trace= 1)))
+(RlmSTM11 <- update(RlmST, control = lmrob.control("KS2011", method="MM",                trace= 1)))
+(RlmSTM14 <- update(RlmST, control = lmrob.control("KS2014", method="MM",                trace= 1)))
+## using "biweight" instead of "lqq"  fixes the problem :
+(RlmSTM11b <- update(RlmST,control = lmrob.control("KS2011", method="MM", psi="biweight",trace= 1)))
+(RlmSTM14b <- update(RlmST,control = lmrob.control("KS2014", method="MM", psi="biweight",trace= 1)))
+(RlmST.11b <- update(RlmST,control = lmrob.control("KS2011",              psi="biweight",trace= 1)))
+(RlmST.14b <- update(RlmST,control = lmrob.control("KS2014",              psi="biweight",trace= 1)))
+## NB: RlmST has component 'init.S' the others have "init" -- documented in ?lmrob.fit == ../man/lmrob.fit.Rd
+R.ini.cf <- t(sapply(mget(ls(patt = "^RlmST")), function(OB) OB$init$coef))
+R..cf    <- t(sapply(mget(ls(patt = "^RlmST")), coef))
+cbind(R.ini.cf, R..cf) ##---> "lqq" is *NOT* robust enough here -- but "biweight" is !!
+
+## Directly look at init.S():
+x.s <- model.matrix(~ log.Te, data = starsCYG)
+y.s <- model.response(model.frame(log.light ~ log.Te, data = starsCYG))
+ini.df <- lmrob.S(x.s, y.s, control=lmrob.control())
+ini.11 <- lmrob.S(x.s, y.s, control=lmrob.control("KS2011"))
+ini.14 <- lmrob.S(x.s, y.s, control=lmrob.control("KS2014"))
+## but these are fine !! :
+rbind(deflt = ini.df$coef,
+      KS.11 = ini.11$coef,
+      KS.14 = ini.14$coef)
+
+##==> No, it is *not* the init.S()
+
 
 set.seed(47)
 data(hbk)
